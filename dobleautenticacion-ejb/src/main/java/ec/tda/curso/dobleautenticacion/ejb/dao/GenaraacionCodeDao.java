d6785f153a16a5f4fed461915d458acea470df5f
SantyJosu25 <santy2516@gmail.com>
2019-09-14 13:26:23 -0500

    generacion codigo

---
+++ dobleautenticacion-ejb/src/main/java/ec/tda/curso/dobleautenticacion/ejb/dao/GenaraacionCodeDao.java
@@ -0,0 +1,10 @@
+package ec.tda.curso.dobleautenticacion.ejb.dao;
+
+import ec.tda.curso.dobleautenticacion_ejb.dao.generic.DaoGenerico;
+import ec.tda.curso.dobleautenticacion_ejb.entidades.GeneratorCode;
+import javax.ejb.Local;
+
+@Local
+public interface GenaraacionCodeDao extends DaoGenerico<GeneratorCode> {
+
+}
---
+++ dobleautenticacion-ejb/src/main/java/ec/tda/curso/dobleautenticacion/ejb/dao/GeneracionCodeDaoImpl.java
@@ -0,0 +1,10 @@
+package ec.tda.curso.dobleautenticacion.ejb.dao;
+
+import ec.tda.curso.dobleautenticacion_ejb.dao.generic.DaoGenericoImplements;
+import ec.tda.curso.dobleautenticacion_ejb.entidades.GeneratorCode;
+import javax.ejb.Stateless;
+
+@Stateless
+public class GeneracionCodeDaoImpl extends DaoGenericoImplements<GeneratorCode> implements GenaraacionCodeDao{
+    
+}
