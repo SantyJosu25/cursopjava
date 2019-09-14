f9464ed169544ce41286e55ef3ff3149b9adcceb
SantyJosu25 <santy2516@gmail.com>
2019-09-14 13:10:56 -0500

    MAIL

--- dobleautenticacion-ejb/target/generated-sources/annotations/ec/tda/curso/dobleautenticacion_ejb/entidades/Usuario_.java
+++ dobleautenticacion-ejb/target/generated-sources/annotations/ec/tda/curso/dobleautenticacion_ejb/entidades/Usuario_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-14T11:52:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-14T13:03:13")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
